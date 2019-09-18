aa5be370ba7bcc7e86e6771ec3d965d975c8a8b2
orfiso <orfiso@macs-MacBook-Air-2.local>
2019-09-18 18:52:33 +0900

    - bag/bell/fur/peak/reel/windmill

--- include/common.h
+++ include/common.h
@@ -1,40 +1,45 @@
 #pragma once
 
 // member identity
-#define ID_BELL          (0x1D00 + 0x20)
+#define ID_GAS           (0x1D00 + 0x20)
 #define ID_FLOAT         (0x1D00 + 0x30)
-#define ID_BAG           (0x1D00 + 0x40)
+#define ID_VOICE1        (0x1D00 + 0x40)
 #define ID_LOOK          (0x1D00 + 0x50)
-#define ID_WINDMILL      (0x1D00 + 0x60)
+#define ID_VOICE2        (0x1D00 + 0x60)
 #define ID_GLASS         (0x1D00 + 0x70)
-#define ID_REEL          (0x1D00 + 0x80)
+#define ID_FALL2         (0x1D00 + 0x80)
 #define ID_ARROW         (0x1D00 + 0x90)
 #define ID_THUNDER       (0x1D00 + 0xA0)
-#define ID_FUR           (0x1D00 + 0xB0)
-#define ID_PEAK          (0x1D00 + 0xC0)
+#define ID_FALL1         (0x1D00 + 0xB0)
+#define ID_VOICE3        (0x1D00 + 0xC0)
 #define ID_KEYBED        (0x1D00 + 0xD0)
+#define ID_ANI1          (0x1D00 + 0xE0)
+#define ID_ANI2          (0x1D00 + 0xF0)
 // (special agent)
-#define ID_MONITOR       (0x1E00 + 0x10)
+#define ID_MONITOR        (0x1E00 + 0x10)
 // (groups)
 #define ID_EVERYONE      (0x1F00 + 0x00)
 // (choice)
-#define IDENTITY         ID_WINDMILL
+#define IDENTITY         ID_ANI1
 
-#define NUM_OF_MEMBERS 13
+
+#define NUM_OF_MEMBERS 15
 int memberList[NUM_OF_MEMBERS] = {
-  ID_BELL, // 1
+  ID_GAS, // 1
   ID_FLOAT, // 2
-  ID_BAG, // 3
+  ID_VOICE1, // 3
   ID_LOOK, // 4
-  ID_WINDMILL, // 5
+  ID_VOICE2, // 5
   ID_GLASS, // 6
-  ID_REEL, // 7
+  ID_FALL2, // 7
   ID_ARROW, // 8
   ID_THUNDER, // 9
-  ID_FUR, // 10
-  ID_PEAK, // 11
+  ID_FALL1, // 10
+  ID_VOICE3, // 11
   ID_KEYBED, // 12
-  ID_MONITOR // 13
+  ID_MONITOR, // 13
+  ID_ANI1, // 14
+  ID_ANI2 // 15
 };
 
 //
@@ -50,7 +55,6 @@ extern painlessMesh mesh;
 // mood
 #define MOOD_LOW            (0x300D0001)
 #define MOOD_HIGH           (0x300D0002)
-#define MOOD_SLEEP          (0x300D0003)
 
 // firmata connectivity
 #define FIRMATA_ON          (0xF13A0001)
@@ -64,21 +68,18 @@ extern painlessMesh mesh;
 //NOTE: disabling AP beacon for speaker accompanied devices!
 #define NODE_TYPE_AP_STA    (0x40DE0001)
 #define NODE_TYPE_STA_ONLY  (0x40DE0002)
-#define NODE_TYPE           NODE_TYPE_AP_STA
-#if (IDENTITY == ID_THUNDER || IDENTITY == ID_GPS)
-#undef NODE_TYPE
 #define NODE_TYPE           NODE_TYPE_STA_ONLY
+#if (IDENTITY == ID_GAS || IDENTITY == ID_LOOK || IDENTITY == ID_MONITOR)
+#undef NODE_TYPE
+#define NODE_TYPE           NODE_TYPE_AP_STA
 #endif
 
 // board
 #define BOARD_NODEMCU_ESP12E (0xBD00 + 1)
-#define BOARD_NODEMCU_ESP32  (0xBD00 + 2)
+#define BOARD_NODEMCU_ESP12N (0xBD00 + 2)
+#define BOARD_NODEMCU_ESP32  (0xBD00 + 3)
 //(choice)
 #define BOARD_SELECT BOARD_NODEMCU_ESP12E
-#if (IDENTITY == ID_MONITOR)
-#undef BOARD_SELECT
-#define BOARD_SELECT         BOARD_NODEMCU_ESP32
-#endif
 
 // mesh
 #define MESH_SSID "darker-than-the-navel"
